528762298e9863b699af7d0f3fe12377949ee358
madaracelio <madaracelio@gmail.com>
2021-02-25 16:33:53 +0100

    Ajout des données de jeux vidéo

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Correspondance_mesh_cnn
+
+## Contenu des dossier
+data/ (Images de mesh)
+    Femme/ 
+    Homme/
+
+mesh/ (Fichier .obj corréspondant à chaque image)
+    Femme/
+    Homme/
+
+mesh_with_class/ (Fichier .obj corréspondant à chaque image de chaque classe)
+
+mult_class_data/ (Données Train, test, label pour chaque classe)
+    Train/
+    Test/
+    labels.csv
+
+multi_data/ (Dossiers avec le nom de chaque classe sans label)
---
+++ label.py
@@ -0,0 +1,22 @@
+id = []
+label = []
+
+import pandas as pd
+
+df = pd.read_csv('multi_class_data/labels.csv')
+
+def getColumnListValue(labels: str):
+    for j in df[labels]:
+        if j not in label:
+            label.append(j)
+    return label
+
+def write2csv(filename: str, col1: asarray, col2: asarray, delimiter=","):
+    assert len(col1) > 0
+    res = ""
+    for i in range(len(col1)):
+        res += col1[i] + delimiter + col2 + "\n"
+    pd.to_csv(filename + ".csv", res)
+
+a = getColumnListValue('name')
+print(a)
